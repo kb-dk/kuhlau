ce38c262aa8a1f0eea5bf676acd449610fc9233e
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2013-01-10 15:13:43 +0000

    program for building
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@12 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+saxon  kaleidakustikon.xml  mei2mup-1.0.4.xsl > kaleidakustikon.mup
+$HOME/sources/mup-6.1/bin/mup kaleidakustikon.mup > kaleidakustikon.ps
+$HOME/sources/mup-6.1/bin/mup kaleidakustikon.mup -m kaleidakustikon.mid
+ps2pdf kaleidakustikon.ps
