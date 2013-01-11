47c38aa572236b4dc2f0279e3f22db1a9ba0c58c
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2013-01-11 12:34:13 +0000

    renaming
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@14 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-java -jar saxon/saxon9.jar  kaleidakustikon.xml  mei2mup-1.0.4.xsl > kaleidakustikon.mup
-$HOME/sources/mup-6.1/bin/mup kaleidakustikon.mup > kaleidakustikon.ps
-$HOME/sources/mup-6.1/bin/mup kaleidakustikon.mup -m kaleidakustikon.mid
-ps2pdf kaleidakustikon.ps
+file=`basename $* .xml`
+
+java -jar saxon/saxon9.jar  $*  mei2mup-1.0.4.xsl > "$file.mup"
+$HOME/sources/mup-6.1/bin/mup "$file.mup" > "$file.ps"
+$HOME/sources/mup-6.1/bin/mup "$file.mup" -m "$file.mid"
+ps2pdf "$file.ps" 
