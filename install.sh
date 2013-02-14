7f3d935265e67f2334a3d30230f97b62a1b35be5
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2013-02-14 15:41:05 +0000

    now able to install stuff in database
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@48 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+# We deploy the documents by sending them to the eXist database
+
 ./load_exist.pl \
     --user admin \
     --password flormelis \
-    --host-port disdev.kb.dk:8080  \
+    --host-port disdev-01.kb.dk:8080  \
     --suffix xml \
     --load ./ \
     --context /exist/rest/db/ \
-    --target kuhlau/kaleidakustikon
+    --target kuhlau/kaleidakustikon/
+
+
+./load_exist.pl \
+    --user admin \
+    --password flormelis \
+    --host-port disdev-01.kb.dk:8080  \
+    --suffix xq \
+    --load ./ \
+    --context /exist/rest/db/ \
+    --target kuhlau/
