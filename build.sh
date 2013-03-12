1b59fb3cf3bdfdf3da7bc67509546fdd2c0832a7
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2013-03-12 10:27:48 +0000

    checking the command line
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@72 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 file=`basename $* .xml`
+xml_file="$file.xml"
 
-java -jar saxon/saxon9.jar  $*  mei2mup-1.0.4.xsl > "$file.mup"
-$HOME/sources/mup-6.1/bin/mup "$file.mup" > "$file.ps"
-$HOME/sources/mup-6.1/bin/mup "$file.mup" -m "$file.mid"
-ps2pdf "$file.ps" 
+if [ -f $xml_file ]; then
+    java -jar saxon/saxon9.jar  $*  mei2mup-1.0.4.xsl > "$file.mup"
+    $HOME/sources/mup-6.1/bin/mup "$file.mup" > "$file.ps"
+    $HOME/sources/mup-6.1/bin/mup "$file.mup" -m "$file.mid"
+    ps2pdf "$file.ps" 
+else
+    echo "need an xml file as an argument for being able to transform it"
+fi
