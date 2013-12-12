572a4286e76ff0147065a923fb59b884fe72a96b
atge <atge@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2013-12-12 19:09:06 +0000

    Added MP3 button and metronome mark input; styled buttons
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@260 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

--- js/main-script.js
+++ js/main-script.js
@@ -130,6 +130,26 @@ function getParams() {
    }
 }
 
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isInRange(n,min,max) {
+  if (isNumber(n))
+  {
+    if (n>max) {
+        alert('Maximum value is 240');
+        return max;
+    }
+    if (n<min) {
+        alert('Minimum value is 60');
+        return min;
+    }
+    return n;    
+  }
+  else return '120';
+}
+
 function init() {
     randomize();
     getParams();
