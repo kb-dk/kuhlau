2a169f84337ea2c36076cb58fd1a312fa9503fbd
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2014-04-15 12:28:47 +0000

    an install script
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@297 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

---
+++ install-web-site.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+$KUHLAU = "/home/kaleidakustikon"
+if[ ! -d "$KUHLAU" ]; then
+    mkdir $KUHLAU
+fi
+
+
+
+tar cvf - `find etc -type f -print | grep -v '\.svn'` | (cd "$KUHLAU" ; tar xvf - )
