5cb4f1600341c97007ba7a532cc831c9032941b8
slu <slu@831b3ce8-fb4d-41d1-bd33-d9ac5c646a95>
2014-04-16 11:39:03 +0000

    see if we can create the cache directly
    git-svn-id: http://code-01.kb.dk/svn/kuhlau/trunk@310 831b3ce8-fb4d-41d1-bd33-d9ac5c646a95

--- install-web-site.sh
+++ install-web-site.sh
@@ -2,12 +2,16 @@
 
 export KUHLAU="/home/kaleidakustikon/"
 export CGI="/kb/httpd/cgi-bin/"
+export USER="apache"
 
 if [ ! -d "$KUHLAU" ]; then
     mkdir "$KUHLAU"
 fi
 
-
+if [ ! -d "$KUHLAU/cache" ]; then
+    mkdir "$KUHLAU"
+    chown "$USER:$USER" "$KUHLAU/cache"
+fi
 
 tar cvf - `find etc -type f -print | grep -v '\.svn'` | \
     (cd "$KUHLAU" ; tar xvf - )
